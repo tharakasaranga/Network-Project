cb0cd20d2bb3db90392a0e161ed09aa1dd6f6c9d
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-02-24 15:37:42 +0530

    updated

--- client-agent/config.py
+++ client-agent/config.py
@@ -7,9 +7,9 @@ CONFIG = {
     'MASTER_IP': os.getenv('MASTER_IP', '10.17.63.28'),
     'MASTER_PORT': int(os.getenv('MASTER_PORT', 5000)),
     'CLIENT_ID': os.getenv('CLIENT_ID', socket.gethostname()),
-    'SCAN_DIRECTORIES': os.getenv('SCAN_DIRS', r'\\MyLaptop\Detwork_Test_Run').split(','),
-    'QUARANTINE_DIR': os.getenv('QUARANTINE_DIR', '/quarantine'),
-    'LOG_DIR': os.getenv('LOG_DIR', '/logs'),
+    'SCAN_DIRECTORIES': os.getenv('SCAN_DIRS', os.path.join(os.path.expanduser('~'), 'Downloads', 'Detwork_Test_Run')).split(','),
+    'QUARANTINE_DIR': os.getenv('QUARANTINE_DIR', os.path.join(os.path.expanduser('~'), 'quarantine')),
+    'LOG_DIR': os.getenv('LOG_DIR', os.path.join(os.path.expanduser('~'), 'logs')),
     'HEARTBEAT_INTERVAL': 30,  
     'RECONNECT_DELAY': 10,  
 }
